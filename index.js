1bc738af8725d6d1bb908ae95d67b336e4917613
arzummammadova <arzuam-af106@code.edu.az>
2026-03-16 16:20:52 +0400

    ;

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/",(req,res)=>{
+    res.send("salam dostlar run olur ")
+})
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
